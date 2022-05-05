db9bad5faab00370d26cd707d24e704aa22d4c53
Samuel Safahi <60489622+samisbakedham@users.noreply.github.com>
2022-05-05 02:02:52 -0700

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://candle.vercel.app';
+export const BASE_URL = 'https://cndlchain.com';
 export const API_URL = 'https://network.cndlchain.com';
 
 export const APP_VERSION = '0.1.1';
